92b6b5cda1a592d3c73dadc262ac82bdfb3843c0
Haitham Kaloub <hkaloub@gmail.com>
2018-12-19 22:50:33 -0500

    Initial Commit

---
+++ WishList2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WishList2
+//
+//  Created by Summer Crow on 19/12/2018.
+//  Copyright © 2018 ghourab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
